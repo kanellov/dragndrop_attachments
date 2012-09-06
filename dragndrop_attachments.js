7bfadbdb2a7fa92243574e8d1261853dd4d6439f
Strimpakos Giorgos <strimpak.geo@gmail.com>
2012-09-06 10:49:05 +0300

    fixed bug - and added localization
    when uploading more than one files per time there was a possibility that
    the session variable is not accurate because of race condition between
    xmlhttprequests. So I made a single http request for multiple files
    upload.

--- dragndrop_attachments.js
+++ dragndrop_attachments.js
@@ -29,13 +29,11 @@ if (window.rcmail) {
 					rcmail.display_message(rcmail.get_label('fileuploaderror'), 'error');
 				return(req);
 				};
-
-				function getBuilder(fname, fdata, bdary) {
+				function startBuilder(bdary)
+				{
 					var dashdash = '--',
 						crlf = '\r\n',
 						builder = '';
-
-					
 					builder += dashdash;
 					builder += bdary;
 					builder += crlf;
@@ -44,6 +42,13 @@ if (window.rcmail) {
 					builder += crlf;
 					builder += document.getElementsByName("_token")[0].value;
 					builder += crlf;
+					return builder;
+				}
+				function getBuilder(fname, fdata, bdary) {
+					var dashdash = '--',
+						crlf = '\r\n',
+						builder = '';
+
 					/* Write boundary. */
 					builder += dashdash;
 					builder += bdary;
@@ -59,6 +64,15 @@ if (window.rcmail) {
 					builder += fdata;
 					builder += crlf;
 			        
+					
+					return builder;
+				}
+				function stopBuilder(bdary)
+				{
+					var dashdash = '--',
+						crlf = '\r\n',
+						builder = '';
+
 					builder += dashdash;
 					builder += bdary;
 					builder += dashdash;
@@ -70,30 +84,73 @@ if (window.rcmail) {
 				event.preventDefault();
 				$('#compose-attachments').css("background-color", "#fff");
 				data = event.dataTransfer;
+				if(data.files.length > 4)
+				{
+					alert(rcmail.gettext('too_many_files_to_upload','dragndrop_attachments'));
+					return false;
+				}
+				var total_size = 0;
+				for(var l = 0; l < data.files.length; l++)
+					total_size = total_size + data.files[l].size;
+				if(total_size > 10000000)
+				{
+					alert(rcmail.gettext('files_to_upload_bigger_than_allowed','dragndrop_attachments'));
+					return false;
+				}
+				var boundary = '----WebKitFormBoundary' + (new Date).getTime();
+				var builder = '';
+				var readyToGo = data.files.length;
+				
+				var reader;
+				//console.log(builder);
+			    var ts = new Date().getTime(),
+							    frame_name = 'rcmupload'+ts;
+                                                        var id = ts;
+                                                        var files=data.files;
+                                                     
+							var action = rcmail.url("upload", { _id:rcmail.env.compose_id||'', _uploadid:ts });
+							var content = '<span>' + rcmail.get_label('uploading' + (files.length > 1 ? 'many' : '')) + '</span>',
+						        ts = frame_name.replace(/^rcmupload/, '');
+
+						      if (rcmail.env.loadingicon)
+						        content = '<img src="'+rcmail.env.loadingicon+'" alt="" class="uploading" />'+content;
+						      content = '<a title="'+rcmail.get_label('cancel')+'" onclick="return rcmail.cancel_attachment_upload(\''+ts+'\', \''+frame_name+'\');" href="#cancelupload" class="cancelupload">'
+						        + (rcmail.env.cancelicon ? '<img src="'+rcmail.env.cancelicon+'" alt="" />' : rcmail.get_label('cancel')) + '</a>' + content;
+						      
+						      rcmail.add2attachment_list(ts, { name:'', html:content, classname:'uploading', complete:false });
+
 				for (var i = 0; i < data.files.length; i++) {
-						var reader = new FileReader();
+						reader = new FileReader();
 						reader.data = data;
 						reader.file = data.files[i];
 						var file = data.files[i];
 						reader.name = data.files[i].name;
-						reader.onload = function(e) {
-							
-							var ts = new Date().getTime(),
-							    frame_name = 'rcmupload'+ts;
-                                                        var id = ts;
+						reader.readAsBinaryString(file);
+						reader.onload = read_done;
+							 
+				}
+				function read_done(event)
+				{
+					
+					builder = builder + getBuilder(event.target.name, event.target.result, boundary );
+					
+					readyToGo --;
 
-							var action = rcmail.url("upload", { _id:rcmail.env.compose_id||'', _uploadid:ts });
-
-							var boundary = '----WebKitFormBoundary' + (new Date).getTime();
-							/* Build RFC2388 string. */
-							var builder = '';
+					if(readyToGo == 0)
+					{
+						sendAllFiles(builder,boundary);
+						
+					}
+				}
+				function sendAllFiles(datastring,mybdary)
+				{
+					var boundary = mybdary;
+					var builder = startBuilder(boundary);
+					builder = builder + datastring;
+					builder = builder + stopBuilder(boundary);
 							var xhr = createInstance();
-							builder = getBuilder(e.target.name, e.target.result, boundary);
-
-						    xhr.open("POST", action, true);
+							xhr.open("POST", action, true);
 							xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' + boundary);
-							xhr.onload = upload_finish;
-							
 							xhr.sendAsBinary = function(datastr) {
 								function byteValue(x) {
 							        return x.charCodeAt(0) & 0xff;
@@ -102,86 +159,39 @@ if (window.rcmail) {
 							    var ui8a = new Uint8Array(ords);
 							    this.send(ui8a.buffer);
 							}
-
 							xhr.sendAsBinary(builder);
-							var files=data.files;
-							
-							var content = '<span>' + rcmail.get_label('uploading' + (files > 1 ? 'many' : '')) + '</span>',
-						        ts = frame_name.replace(/^rcmupload/, '');
 
-						      if (rcmail.env.loadingicon)
-						        content = '<img src="'+rcmail.env.loadingicon+'" alt="" class="uploading" />'+content;
-						      content = '<a title="'+rcmail.get_label('cancel')+'" onclick="return rcmail.cancel_attachment_upload(\''+ts+'\', \''+frame_name+'\');" href="#cancelupload" class="cancelupload">'
-						        + (rcmail.env.cancelicon ? '<img src="'+rcmail.env.cancelicon+'" alt="" />' : rcmail.get_label('cancel')) + '</a>' + content;
-
-						      rcmail.add2attachment_list(ts, { name:'', html:content, classname:'uploading', complete:false });
-
-							
-						}
-						reader.readAsBinaryString(file);
-					
-
+							xhr.onreadystatechange = function() {
+        							if(xhr.readyState == 4){
+									var mycontent = '';
+				          			mycontent = xhr.responseText;
+				      				if (!mycontent.match(/add2attachment/) ) {
+									if (!mycontent.match(/display_message/))
+										rcmail.display_message(rcmail.get_label('fileuploaderror'), 'error');
+									}
+									var from = mycontent.search('rcmfile');
+									var to = from + 31;
+									var myframe_name = mycontent.substring(from, to);
+								    var frame = document.createElement('iframe');
+								    frame.name = myframe_name;
+								    frame.style.border = 'none';
+								    frame.style.width = 0;
+								    frame.style.height = 0;
+								    frame.style.visibility = 'hidden';
+								    document.body.appendChild(frame);
+				    				var doc = frame.document;
+									if(frame.contentDocument)
+										doc = frame.contentDocument; // For NS6
+									else if(frame.contentWindow)
+										doc = frame.contentWindow.document; // For IE5.5 and IE6
+									// Put the content in the iframe
+									doc.open();
+									doc.writeln(mycontent);
+									doc.close();
+									rcmail.display_message(rcmail.gettext('all_files_successfully_uploaded', 'dragndrop_attachments'), 'confirmation');
+								}
+				         }
 				}
-
-				function upload_finish(event) {
-					var d, content = '';
-					try {
-						
-						 
-				          if (this.contentDocument) {
-				            d = this.contentDocument;
-				            content = d.childNodes[0].innerHTML;
-				          } else if (this.contentWindow) {
-				            d = this.contentWindow.document;
-				            content = d.childNodes[0].innerHTML;
-				          }
-				          else{
-				          content = this.responseText;
-				      	  }
-
-				      if (!content.match(/add2attachment/) ) {
-							if (!content.match(/display_message/))
-								rcmail.display_message(rcmail.get_label('fileuploaderror'), 'error');
-							rcmail.remove_from_attachment_list(e.data.ts);
-						}
-					var from = content.search('rcmfile');
-					var to = from + 31;
-					var frame_name = content.substring(from, to);
-			        
-			        if (document.all) {
-				      var html = '<iframe name="'+frame_name+'" src="program/blank.gif" style="width:0;height:0;visibility:hidden;"></iframe>';
-				      document.body.insertAdjacentHTML('BeforeEnd', html);
-				    }
-				    else { // for standards-compilant browsers
-				      var frame = document.createElement('iframe');
-				      frame.name = frame_name;
-				      frame.style.border = 'none';
-				      frame.style.width = 0;
-				      frame.style.height = 0;
-				      frame.style.visibility = 'hidden';
-				      document.body.appendChild(frame);
-				    }
-
-				    var doc = frame.document;
-						if(frame.contentDocument)
-							doc = frame.contentDocument; // For NS6
-						else if(frame.contentWindow)
-							doc = frame.contentWindow.document; // For IE5.5 and IE6
-						// Put the content in the iframe
-						doc.open();
-						doc.writeln(content);
-						doc.close();
-				          
-        
-					} catch (err) {
-
-						
-					}
-					
-
-				};
-
-
 				/* Prevent FireFox opening the dragged file. */
 				event.stopPropagation();
 
